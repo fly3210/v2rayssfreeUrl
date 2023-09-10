e82aeb9cc2db416e5890d2467e9fae0f56a5679e
fly321 <luoliyaa@hotmail.com>
2023-09-10 23:15:15 +0800

    3333

--- main.go
+++ main.go
@@ -102,7 +102,7 @@ func GetV2rayString() string {
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
-	fmt.Println(res.Body)
+	fmt.Println(string(body))
 	if err != nil {
 		fmt.Println(err)
 	}
